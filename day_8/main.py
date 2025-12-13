d5be1fc19cd2245a48ecbbb303d52aa34d129a81
tombe <mail@thomas-ruff.de>
2025-12-14 00:59:07 +0100

    Day 8

---
+++ day_8/main.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Coordinate:
+    x: int
+    y: int
+    z: int
+
+    def distance(self, other):
+        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2) ** 0.5
+
+with open('input.txt', 'r') as f:
+    data = f.read().splitlines(keepends=False)
+
+data = [Coordinate(*[int(i) for i in row.split(',')]) for row in data]
+
+distances: list[tuple[tuple[tuple[int, int, int], tuple[int, int, int]], float]] = []
+
+for i, c_1 in enumerate(data):
+    for j, c_2 in enumerate(data[i + 1:]):
+        distances.append((((c_1.x, c_1.y, c_1.z), (c_2.x, c_2.y, c_2.z)), c_1.distance(c_2)))
+
+distances.sort(key=lambda x: x[1])
+
+clusters: list[set[tuple[tuple[int, int, int], tuple[int, int, int]]]] = []
+connected = 0
+for (c_1, c_2), dist in distances:
+    print(f'>> Checking {(c_1, c_2)} with distance {dist}')
+    found_clusters = []
+    for i, cluster in enumerate(clusters):
+        if len(cluster) > 1:
+            cluster_indexes = set.union(*[set(c) for c in cluster])
+        else:
+            cluster_indexes = set(next(iter(cluster)))
+        if c_1 in cluster_indexes and c_2 in cluster_indexes:
+            print(f'{c_1[0]} and {c_2[1]} already in cluster {cluster}')
+            break
+        if c_1 in cluster_indexes or c_2 in cluster_indexes:
+            found_clusters.append(i)
+            if len(found_clusters) == 2:
+                break
+    if len(found_clusters) == 1:
+        print(f'{(c_1, c_2)} added to cluster {clusters[found_clusters[0]]}')
+        clusters[found_clusters[0]].add((c_1, c_2))
+    elif len(found_clusters) == 2:
+        print(f'{(c_1, c_2)} merged clusters {clusters[found_clusters[0]]} and {clusters[found_clusters[1]]}')
+        clusters[found_clusters[0]].update(clusters[found_clusters[1]])
+        clusters[found_clusters[0]].add((c_1, c_2))
+        del clusters[found_clusters[1]]
+    if len(found_clusters) == 0:
+        clusters.append({(c_1, c_2)})
+        print(f'{(c_1, c_2)} created new cluster')
+    connected += 1
+    if connected == 1000:
+        break
+
+print(clusters)
+clusters_with_coordinates = [set.union(*[set(c) for c in cluster]) for cluster in clusters]
+cluster_sizes = sorted([len(c) for c in clusters_with_coordinates], reverse=True)
+
+print(cluster_sizes[0] * cluster_sizes[1] * cluster_sizes[2])
+
+
+
+
